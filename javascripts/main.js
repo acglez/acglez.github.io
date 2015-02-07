93ab96f943b63583db4f7a9e6bf99ea48c22f188
Adrian <acglez10@gmail.com>
2015-02-07 01:25:54 +0000

    Conseguido cambiar imagenes mouseover

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+function cambiar_node () {
+  document.getElementById('ventana_img').src = "images/node.png";
+}
+
+function cambiar_temp () {
+  document.getElementById('ventana_img').src = "images/temperatura.jpg";
+}
+
+function volver () {
+  document.getElementById('ventana_img').src = "images/github-logo.jpg";
+}
